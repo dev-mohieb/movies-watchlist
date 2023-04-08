dd1a2c9162226c7570a0275d34afea6a81a67f26
Mohieb <dev.mohieb@gmail.com>
2023-04-08 20:12:48 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
         'header-l': "url('/src/images/header-l.jpg')",
         'header-m': "url('/src/images/header-m.jpg')",
         'header-s': "url('/src/images/header-s.jpg')"
+      },
+      backgroundColor: {
+        'main-dark': '#121212',
+        'accent-dark': '#2E2E2F',
+        'accent': '#DFDDDD'
+      },
+      textColor: {
+        'main-dark': '#121212',
+        'accent-dark': '#2E2E2F',
+        'accent': '#DFDDDD',
+        'subtitles': '#111827',
+        'star': '#FEC654',
+        'desc': '#6B7280',
+        'desc-dark': '#A5A5A5'
       }
     },
   },
