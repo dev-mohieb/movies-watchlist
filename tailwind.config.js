08999f8bc7f64c0b8c00f30ff951824ec50ddc48
Mohieb <dev.mohieb@gmail.com>
2023-04-14 20:29:07 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,29 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'inter': 'Inter, sans-serif'
+        inter: "Inter, sans-serif",
       },
       backgroundImage: {
-        'header-l': "url('/images/header-l.jpg')",
-        'header-m': "url('/images/header-m.jpg')",
-        'header-s': "url('/images/header-s.jpg')"
+        "header-l": "url('/images/header-l.jpg')",
+        "header-m": "url('/images/header-m.jpg')",
+        "header-s": "url('/images/header-s.jpg')",
       },
       backgroundColor: {
-        'main-dark': '#121212',
-        'accent-dark': '#2E2E2F',
-        'accent': '#DFDDDD'
+        "main-dark": "#121212",
+        "accent-dark": "#2E2E2F",
+        accent: "#DFDDDD",
       },
       textColor: {
-        'main-dark': '#121212',
-        'accent-dark': '#2E2E2F',
-        'accent': '#DFDDDD',
-        'subtitles': '#111827',
-        'star': '#FEC654',
-        'desc': '#6B7280',
-        'desc-dark': '#A5A5A5'
-      }
+        "main-dark": "#121212",
+        "accent-dark": "#2E2E2F",
+        accent: "#DFDDDD",
+        subtitles: "#111827",
+        star: "#FEC654",
+        desc: "#6B7280",
+        "desc-dark": "#A5A5A5",
+      },
     },
   },
   plugins: [],
-   darkMode: 'class'
-}
+  darkMode: "class",
+};
