4127b1d8bfc382d87e09d93a5d293f075dac75b2
Mohieb <dev.mohieb@gmail.com>
2023-04-14 20:29:54 +0300

    refactoring - add loading state - add light mode func

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,8 +1,3 @@
-import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-import { moviesArr, titlesArr } from "../index.js";
-
-const KEY = "ff27a997";
-const main = document.querySelector("main");
 const errorEl = `
     <article
       id="start-exploring"
@@ -12,8 +7,19 @@ const errorEl = `
       </p>
     </article>
     `;
+const loading = `
+    <article
+    class="m-auto text-center text-lg text-accent dark:text-accent-dark md:text-xl lg:text-2xl">
+      <i class="fa-solid fa-spinner fa-spin text-6xl md:text-7xl lg:text-8xl"></i>
+    </article>
+    `;
+const moviesArr = [];
+const titlesArr = [];
+const KEY = "ff27a997";
+const main = document.querySelector("main");
 
 async function fetchMoviesTitles(search, num) {
+  main.innerHTML = loading;
   // fetch 10 movie titles using the ' ?s= ' query
   const res = await fetch(
     `https://www.omdbapi.com/?s=${search}&apikey=${KEY}&page=${num}`
@@ -21,7 +27,6 @@ async function fetchMoviesTitles(search, num) {
   const movies = await res.json();
   // push titles into array of titles
   if (movies.Response === "False") {
-    main.classList.add("justify-center");
     main.innerHTML = errorEl;
   } else {
     for (let movie of movies.Search) {
@@ -35,25 +40,15 @@ async function fetchMoviesTitles(search, num) {
 async function fetchMoviesDetails(titles) {
   // loop over each titles and fetch each movie individually using the ' ?t= ' query
   for (let title of titles) {
-    const res = await fetch(`https://www.omdbapi.com/?t=${title}&apikey=${KEY}`);
+    const res = await fetch(
+      `https://www.omdbapi.com/?t=${title}&apikey=${KEY}`
+    );
     const movieWithDetails = await res.json();
-    // create new movie objects based on the recieved movies to add two
-    // properties: a uuid, and a isInWatchlist boolean
-    const newMovieObj = new Movie(movieWithDetails);
     // push newly made movie objects to moviesArr
-    moviesArr.push(newMovieObj);
+    moviesArr.push(movieWithDetails);
   }
   // render movies using moviesArr
   renderMovies(moviesArr);
-}
-
-class Movie {
-  constructor(data) {
-    Object.assign(this, data);
-    // add two proprties to each movie
-    this.uuid = uuidv4();
-    this.isInWatchlist = false;
-  }
 }
 
 function getMoviesHtml(movies, btn) {
@@ -67,16 +62,16 @@ function getMoviesHtml(movies, btn) {
         Plot: plot,
         Poster: poster,
         imdbRating: rating,
-        uuid,
+        imdbID,
       } = movie;
 
       return `
       <section
-      class="flex h-[212px] items-center gap-6 border-b border-[#E5E7EB] py-8 text-subtitles dark:border-[#2C2C2C] dark:text-white px-2 sm:px-4 sm:h-[234px] md:h-[254px] lg:h-[274px]"
+      class="flex h-[212px] items-center gap-3 border-b border-[#E5E7EB] py-8 text-subtitles dark:border-[#2C2C2C] dark:text-white px-2 sm:px-4 sm:h-[234px] md:h-[254px] lg:h-[274px]"
       >
       <img
         class="h-full w-[105px] rounded object-cover sm:w-[125px] md:w-[145px] lg:w-[160px]"
-        src="${poster}"
+        src="${poster === "N/A" ? "/images/no-image-placeholder.png" : poster}"
         width="100"
         height="212"
         alt=""
@@ -98,17 +93,18 @@ function getMoviesHtml(movies, btn) {
           <p class="">${runtime}</p>
           <p class="">${genre}</p>
           <button
-            data-uuid="${uuid}"
-            class="flex cursor-default items-center gap-[6px] text-sm md:cursor-pointer"
+            data-imdb-id="${imdbID}"
+            class="flex cursor-default items-center gap-[6px] text-sm md:cursor-pointer px-1"
           >
-            <i class="fa-solid fa-circle-${btn} text-lg"></i>
-            Watchlist
+            <i class="fa-solid fa-circle-${btn} text-lg pointer-events-none"></i>
           </button>
         </article>
         <!-- Description -->
-        <p class="max-w-[400px] text-sm text-desc dark:text-desc-dark">
-          ${plot}
-        </p>
+        <section class="max-w-[400px] text-sm text-desc dark:text-desc-dark overflow-hidden text-ellipsis h-[100px]">
+          <p class="">
+            ${plot}
+          </p>
+        </section>
       </section>
     </section>
       `;
@@ -121,4 +117,31 @@ function renderMovies() {
   main.innerHTML = getMoviesHtml(moviesArr, "plus");
 }
 
-export { fetchMoviesTitles, getMoviesHtml };
+// Seperate functions for each mode - might refactor in the future
+function lightMode() {
+  localStorage.setItem("mode", "light");
+  document.querySelector("html").classList.remove("dark");
+  document.querySelector("#toggle > i").classList.add("fa-sun");
+  document.querySelector("#toggle > i").classList.remove("fa-moon");
+}
+function darkMode() {
+  localStorage.setItem("mode", "dark");
+  document.querySelector("html").classList.add("dark");
+  document.querySelector("#toggle > i").classList.add("fa-moon");
+  document.querySelector("#toggle > i").classList.remove("fa-sun");
+}
+function handleToggle() {
+  document.querySelector("html").classList.contains("dark")
+    ? lightMode()
+    : darkMode();
+}
+
+export {
+  fetchMoviesTitles,
+  getMoviesHtml,
+  moviesArr,
+  titlesArr,
+  handleToggle,
+  darkMode,
+  lightMode,
+};
