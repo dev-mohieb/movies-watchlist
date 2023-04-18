bff7c1acd28e63bc2a48839b61dc87afc7e90f39
Mohieb <105434369+dev-mohieb@users.noreply.github.com>
2023-04-18 14:55:56 +0300

    Update utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -76,7 +76,7 @@ function getMoviesHtml(movies, btn) {
         width="100"
         height="212"
         alt=""
-        style="aspect-ratio: 10/15; max-width: 165px;"
+        style="aspect-ratio: 10/15; max-width: 165px; height: 90%;"
       />
       <!-- Movie info -->
       <section class="h-full max-w-[400px] w-[90%] overflow-y-auto space-y-1 scrollbar">
